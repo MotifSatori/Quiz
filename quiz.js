b92e604e5bbbe14fd465e844f2751f9a6b899d38
Motif Satori <bigo77577@gmail.com>
2021-02-25 20:01:49 -0500

    10 new questions

--- quiz.js
+++ quiz.js
@@ -10,6 +10,16 @@ function check() {
     var q8=document.quiz.question8.value;
     var q9=document.quiz.question9.value;
     var q10=document.quiz.question10.value;
+    var q11=document.quiz.question11.value;
+    var q12=document.quiz.question12.value;
+    var q13=document.quiz.question13.value;
+    var q14=document.quiz.question14.value;
+    var q15=document.quiz.question15.value;
+    var q16=document.quiz.question16.value;
+    var q17=document.quiz.question17.value;
+    var q18=document.quiz.question18.value;
+    var q19=document.quiz.question19.value;
+    var q20=document.quiz.question20.value;
     var result=document.getElementById("result");
     var quiz=document.getElementById("quiz");
     if (q1=="Swinging wide") {correct++}
@@ -22,13 +32,25 @@ function check() {
     if (q8=="From 1/2 an hour after sunset to 1/2 an hour before sunrise") {correct++}
     if (q9=="Back up") {correct++}
     if (q10=="You may proceed through the intersection") {correct++}
+    if (q11=="Pull over the right edge of the roadway") {correct++}
+    if (q12=="Stop sign") {correct++}
+    if (q13=="Pass along the shoulder") {correct++}
+    if (q14=="Both A and B") {correct++}
+    if (q15=="Exceed the speed limit") {correct++}
+    if (q16=="Go right to take Exit 44") {correct++}
+    if (q17=="handicapped parking zone") {correct++}
+    if (q18=="Come to a complete stop and check your surroundings") {correct++}
+    if (q19=="No U-turns allowed") {correct++}
+    if (q20=="Railroad crossing") {correct++}
     quiz.style.display="none";
+    head.style.display = "none";
+
   
-     if (correct<=7) {
-         result.textContent= 'You Failed :( Please refresh & try again.';
-         result.innerHTML += '<img src = "https://www.digitalartsonline.co.uk/cmsdata/features/3782497/05.gif">';
+     if (correct<=14) {
+         result.textContent= 'You Can Do Better :( Refresh & try again.';
+         result.innerHTML += '<img src = "https://www.digitalartsonline.co.uk/cmsdata/features/3782497/05.gif" width = "400" height = "400">';
      } else {
          result.textContent= 'You Passed :) You are ready for the real thing! '; 
-         result.innerHTML += '<img src = "https://www.digitalartsonline.co.uk/cmsdata/features/3782497/04.gif">';
+         result.innerHTML += '<img src = "https://www.digitalartsonline.co.uk/cmsdata/features/3782497/04.gif" width = "400" height = "400">';
      }
 }
